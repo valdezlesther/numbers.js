95beaedf25522bb83030f8de4a58988cf629231c
valdezlesther <marklesthervaldez52@gmail.com>
2024-09-13 19:53:11 +0800

    Create numbers.js

---
+++ numbers.js
@@ -0,0 +1,9 @@
+let firstFavNum = 25;
+let secondFavNum = 17;
+
+console.log(firstFavNum + secondFavNum);
+console.log(firstFavNum - secondFavNum);
+console.log(firstFavNum * secondFavNum);
+console.log(firstFavNum / secondFavNum);
+console.log(firstFavNum % secondFavNum);
+console.log(firstFavNum ** secondFavNum);
